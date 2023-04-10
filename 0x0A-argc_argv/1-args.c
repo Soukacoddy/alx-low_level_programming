8a767fb2909b1202c93c7e85a5bab9fad9e20fb3
soukaina <soukainanutrifeed@gmail.com>
2023-04-10 05:22:07 -0700

    task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - program that prints number of arguments
+ * Owned By SOUka
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: number
+ */
+
+int	main(int argc, char *argv[])
+{
+	(void)argv;
+	printf("%d\n", argc - 1);
+	return (0);
+}
