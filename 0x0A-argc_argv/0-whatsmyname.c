ee764afe9e8888afc17147aab3a33a28406d10fc
soukaina <soukainanutrifeed@gmail.com>
2023-04-10 05:16:09 -0700

    task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - program that prints its name
+ * Owned By SOUka
+ * @argv: argument
+ * @argc: argument count
+ * Return: 0
+ */
+
+int	main(int argc, char *argv[])
+{
+	int i = 0;
+
+	while (argv[0][i] != '\0' && argc)
+	{
+		_putchar(argv[0][i]);
+		++i;
+	}
+	_putchar('\n');
+	return (0);
+}
