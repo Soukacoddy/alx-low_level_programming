3bd736e9d95d86f61cfd475934de7b4d144b9f73
soukaina <soukainanutrifeed@gmail.com>
2023-04-17 10:22:08 -0700

    task 0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,17 +1,12 @@
-#ifndef _DOG_H
-#define _DOG_H
-#include <stdio.h>
-#include <stdlib.h>
-
-typedef struct dog dog;
+#ifndef _DOG_H_
+#define _DOG_H_
 
 /**
- * struct dog - check the code
- * @name: pram
- * @age: pram
- * @owner: pram
- *
- * Description: dog struct
+ * struct dog - struct describ a dog
+ * Owned By MoOka
+ * @name:  name of the dog
+ * @age: age of the dog
+ * @owner: owner
  */
 
 struct dog
@@ -20,11 +15,14 @@ struct dog
 	float age;
 	char *owner;
 };
+
 typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
-void init_dog(struct dog *d);
+void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
+
+
 
 #endif
